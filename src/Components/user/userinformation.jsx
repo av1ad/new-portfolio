97185d80fb1794a13844fafbd8349f8875e8c428
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-03-19 17:43:05 -0400

    Added resume button linking to my resume

--- src/Components/user/userinformation.jsx
+++ src/Components/user/userinformation.jsx
@@ -15,8 +15,9 @@ const UserInformation = () => {
         <img className='user-card__image__photo' src={userPicture} alt='Picture of Aviad Churaman' tabIndex="-1"/>
       </section>
       <section className='user-card__footer'>
-        <a href="mailto:ronald.harsh@protonmail.com" className='user-card__footer__email'>aviadchuraman@gmail.com</a>
+        <a href="mailto:aviadchuraman@gmail.com" className='user-card__footer__email'>aviadchuraman@gmail.com</a>
         <p className='user-card__footer__based'>Based in USA</p>
+        <a href="https://drive.google.com/file/d/1DJIqYbHZUKw-Mbj2PTeMoA4-OgDNCLAn/view?usp=sharing" className='user-card__footer__resume'>Resume</a>
         <p className='user-card__footer__copyright'>© 2024 Aviad Churaman; All rights reserved.</p>
       </section>
     </div>
